aa3b61245e2d702aa04b3f857949453cd01463f5
h-nakayama <light.tree.1.13@gmail.com>
2016-10-10 17:53:04 +0900

    add player class

--- client.py
+++ client.py
@@ -3,25 +3,26 @@ import enum
 from src.connection import Connection
 from src.option_parser import OptParser
 from src.cards import Cards
-from src.strategy import Strategy
+from src.strategy import StrategyFactory
 from src.hand import Hand
 from src.table import Table
 
 
-class Game(enum.Enum):
-    cont = 0
-    comp = 1
-    over = 2
+class Game(object):
+    _continue = 0
+    _over = 2
 
+    def __init__(self):
+        self._end_flag = self._continue
 
-def update_field(conn):
-    end_flag = conn.recv_int()
-    return Game(end_flag)
+    def continues(self):
+        return self._end_flag == self._continue
 
+    def is_over(self):
+        return self._end_flag == self._over
 
-def update_end_flag(conn):
-    end_flag = conn.recv_int()
-    return Game(end_flag)
+    def set_end_flag(self, flag):
+        self._end_flag = flag
 
 
 def run():
@@ -29,25 +30,27 @@ def run():
     with Connection(addr, port) as conn:          # サーバーに接続
         conn.send_name(name)                      # サーバーに名前を送信する
         player_id = conn.recv_int()               # プレイヤーidを受け取る
-        end_flag = Game.cont
-        while end_flag is not Game.over:
+        game = Game()
+        while not game.is_over():
             # Before a game
             data = conn.recv_table()                             # 手札を受け取る
             hand = Hand(data)
             table = Table(data)
             if table.is_exchange and table.exchange_num > 0:      # カード交換をする場合は
-                strategy = Strategy(hand, table, Cards([]))   # カード選択のための戦略クラスを生成する
-                cards = strategy.select_change_cards()            # カード交換用のカードを選択する
+                factory = StrategyFactory(hand, table)   # カード選択のための戦略クラスを生成する
+                strategy = factory.create()
+                cards = strategy.select_cards()            # カード交換用のカードを選択する
                 data = cards.decode()
                 conn.send_table(data)                            # 選択したカードを提出する
-            card_state = Cards([])
-            while end_flag is Game.cont:
+
+            while game.continues():
                 # During a game
                 data = conn.recv_table()                         # 手札と場の状態を受け取る
                 hand = Hand(data)
                 table = Table(data)
                 if table.is_my_turn:                              # 自分の番だったら
-                    strategy = Strategy(hand, table, card_state)  # カード選択のための戦略クラスを生成する
+                    factory = StrategyFactory(hand, table)   # カード選択のための戦略クラスを生成する
+                    strategy = factory.create()
                     cards = strategy.select_cards()               # カードを選択する
                     data = cards.decode()
                     conn.send_table(data)                        # 選択したカードを提出する
@@ -57,8 +60,9 @@ def run():
                 field_cards = Hand(data)
                 card_state = Cards(field_cards)               # 場のカード状態を決定する
 
-                update_field(conn)
-                end_flag = update_end_flag(conn)
+                data = conn.recv_table()  # update field
+                end_flag = conn.recv_int()
+                game.set_end_flag(end_flag)
 
 
 if __name__ == '__main__':
--- src/table.py
+++ src/table.py
@@ -5,36 +5,6 @@ class Table(object):
     pass
 
 
-class Players(object):
-    """
-    プレイヤーたちの状態を表すクラス
-    """
-
-    def __init__(self, data):
-        self._is_exchange    = bool(data[5][0])  # カード交換中か
-        self._exchange_num   = data[5][1]        # カード交換の枚数
-        self._is_my_turn     = bool(data[5][2])  # 自分のターンか
-        self._current_player = data[5][3]        # 何番目のプレイヤーのターンか
-        self._is_empty       = bool(data[5][4])  # 場が流れたか
-        self._card_num       = data[6][0:5]      # プレイヤーの手札枚数
-        self._class          = data[6][5:10]     # プレイヤーの階級
-        self._seats          = data[6][10:15]    # どこに誰が座っているか
-
-    def __str__(self):
-        s = """
-        is_exchange   : {}
-        exchange_num  : {}
-        is_my_turn    : {}
-        current_player: {}
-        is_empty      : {}
-        card_num      : {}
-        class         : {}
-        seats         : {}
-        """.format(self._is_exchange, self._exchange_num, self._is_my_turn, self._current_player,
-                   self._is_empty, self._card_num, self._class, self._seats)
-        return s
-
-
 class TableEffect(object):
     """
     場にかかっている効果を表すクラス
