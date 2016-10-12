0ce82c1b9a60e291ad271501cc541a48ce2bc44a
Hironsan <light.tree.1.13@gmail.com>
2016-10-12 10:56:14 +0900

    add player testcase

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
-from src.player import Players
+from src.player import Player, Players
 
 
 class PlayersTest(unittest.TestCase):
@@ -9,3 +9,38 @@ class PlayersTest(unittest.TestCase):
         table = [[0] * 15 for i in range(8)]
         players = Players(table)
         print(players)
+
+
+class PlayerTest(unittest.TestCase):
+
+    def test_needs_exchange(self):
+        data = [[0] * 15 for i in range(8)]
+        data[5][0] = 1  # カード交換中か
+        data[5][1] = 1  # カード交換の枚数
+        player = Player(data)
+        self.assertTrue(player.needs_exchange())
+
+        data[5][1] = 2
+        player = Player(data)
+        self.assertTrue(player.needs_exchange())
+
+        data[5][1] = 0
+        player = Player(data)
+        self.assertFalse(player.needs_exchange())
+
+        data[5][0] = 0
+        player = Player(data)
+        self.assertFalse(player.needs_exchange())
+
+        data[5][1] = 1
+        player = Player(data)
+        self.assertFalse(player.needs_exchange())
+
+    def test_can_submit(self):
+        data = [[0] * 15 for i in range(8)]
+        player = Player(data)
+        self.assertFalse(player.can_submit())
+
+        data[5][2] = 1
+        player = Player(data)
+        self.assertTrue(player.can_submit())
